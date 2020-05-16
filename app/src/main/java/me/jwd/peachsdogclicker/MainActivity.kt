0d62260cd7e9777c5b9ae0080df9ad2ed1e0c533
Jared De Blander <jared0x90@gmail.com>
2020-05-16 14:25:15 -0400

    Updated peach photo

--- app/src/main/java/me/jwd/peachsdogclicker/MainActivity.kt
+++ app/src/main/java/me/jwd/peachsdogclicker/MainActivity.kt
@@ -1,12 +1,23 @@
 package me.jwd.peachsdogclicker
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mp: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mp = MediaPlayer.create(this, R.raw.click64)
+
+        buttonPlayClicker.setOnClickListener{
+            mp.seekTo(0)
+            mp.start()
+        }
     }
 }
